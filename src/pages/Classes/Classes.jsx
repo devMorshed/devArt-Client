ee26ef3f13734b39c51a0713e1ac069dbab9542a
devMorshed <ac3m0r@gmail.com>
2023-06-08 21:14:48 +0600

    Classes & Instructors

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,9 +1,28 @@
-const Classes = () => {
-  return (
-    <div>
-      classews
-    </div>
-  );
+import { useQuery } from "@tanstack/react-query";
+
+import axios from "axios";
+import SectionHead from "../../components/Shared/SectionHead";
+import ClassCard from "./ClassCard";
+
+const Instructors = () => {
+	const { data } = useQuery(["Classes"], async () => {
+		const res = await axios.get("/classes");
+		return res.data;
+	});
+
+	return (
+		<section className="my-10 dark:bg-gray-800 dark:text-gray-50">
+			<SectionHead
+				heading={" Classes"}
+				subheading={"Choose form our Favorite Instructors"}
+			/>
+			<div className="my-10 grid px-6 md:grid-cols-2 xl:grid-cols-3 gap-6 justify-center items-center">
+				{data?.map((instructor) => (
+					<ClassCard key={instructor.id} data={instructor} />
+				))}
+			</div>
+		</section>
+	);
 };
 
-export default Classes;
+export default Instructors;
--- src/pages/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors.jsx
@@ -1,9 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+
+import axios from "axios";
+import SectionHead from "../../components/Shared/SectionHead";
+import InstructorCard from "./InstructorCard";
+
 const Instructors = () => {
-  return (
-    <div>
-      this is for instructors 
-    </div>
-  );
+	const { data } = useQuery(["Instructors"], async () => {
+		const res = await axios.get("/instructors");
+		return res.data;
+	});
+
+	return (
+		<section className="my-10 dark:bg-gray-800 dark:text-gray-50">
+			<SectionHead
+				heading={" Instructors"}
+				subheading={"Choose form our Favorite Instructors"}
+			/>
+			<div className="my-10 grid px-6 md:grid-cols-2 xl:grid-cols-3 gap-6 justify-center items-center">
+				{data?.map((instructor) => (
+					<InstructorCard key={instructor.id} data={instructor} />
+				))}
+			</div>
+		</section>
+	);
 };
 
 export default Instructors;
