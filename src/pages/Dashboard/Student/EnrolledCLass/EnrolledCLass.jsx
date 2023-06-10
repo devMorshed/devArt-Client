7cbb4078b3604d6f7bb231b194c71de22d46976c
devMorshed <ac3m0r@gmail.com>
2023-06-10 12:40:19 +0600

    Enrolled Class

--- src/pages/Dashboard/Student/EnrolledCLass/EnrolledCLass.jsx
+++ src/pages/Dashboard/Student/EnrolledCLass/EnrolledCLass.jsx
@@ -1,5 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import useAuth from "../../../../hooks/useAuth";
+import SectionHead from "../../../../components/Shared/SectionHead";
+import ClassCard from "../../../Classes/ClassCard";
+
 const EnrolledCLass = () => {
-	return <div>enroled</div>;
+	const [axiosSecure] = useAxiosSecure();
+	const { user } = useAuth();
+
+	const { data, isLoading } = useQuery(["enrolled"], async () => {
+		const res = await axiosSecure.get(`/enrolled?email=${user?.email}`);
+		return res.data;
+	});
+
+	console.log(data, isLoading);
+
+	if (isLoading) {
+		return <div>Loading</div>;
+	}
+
+	if (data) {
+		return (
+			<section className="py-10 dark:bg-gray-800 dark:text-gray-50">
+				<SectionHead
+					heading={"Enrolled Classes"}
+					subheading={"Start Your Journey"}
+				/>
+				<div className="my-10 grid px-6 md:grid-cols-2 xl:grid-cols-3 gap-6 justify-center items-center">
+					{data?.map((item) => (
+						<ClassCard key={item._id} data={item} />
+					))}
+				</div>
+			</section>
+		);
+	}
 };
 
 export default EnrolledCLass;
