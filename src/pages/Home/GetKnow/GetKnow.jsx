c385821d95aaf43c2c9c415e49932cd54e1748e7
devMorshed <ac3m0r@gmail.com>
2023-08-07 13:57:52 +0600

    blurhash

---
+++ src/pages/Home/GetKnow/GetKnow.jsx
@@ -0,0 +1,13 @@
+import LazyImage from "../../../components/Shared/LazyImage";
+const GetKnow = () => {
+  return (
+    <div>
+      <LazyImage
+        src="https://i.ibb.co/28wPjbh/instructor-getknow.jpg"
+        // hash=
+      />
+    </div>
+  );
+};
+
+export default GetKnow;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "./Banner/Banner";
+import GetKnow from "./GetKnow/GetKnow";
 import PopularClassess from "./PopularClasses/PopularClassess";
 import PopularInstructors from "./PopularInstructors/PopularInstructors";
 import Statistics from "./Statistics/Statistics";
@@ -6,10 +7,11 @@ import Statistics from "./Statistics/Statistics";
 const Home = () => {
 	return (
 		<div>
-			<Banner />
-			<PopularClassess />
-			<PopularInstructors />
-			<Statistics />
+			{/* <Banner /> */}
+			<GetKnow/>
+			{/* <PopularClassess /> */}
+			{/* <PopularInstructors /> */}
+			{/* <Statistics /> */}
 		</div>
 	);
 };
